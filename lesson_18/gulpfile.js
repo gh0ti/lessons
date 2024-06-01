844456b47c6f19b2cbd763b1a7fe6ff2b36d1c82
Oleksandr  Berezhynskyi <alexandr.berezhynskyi@bitfocus.com>
2024-06-01 19:05:50 +0300

    added lesson 18 (draft)

---
+++ lesson_18/gulpfile.js
@@ -0,0 +1,32 @@
+import gulp from "gulp";
+import browserSync from "browser-sync";
+import sourceMaps from "gulp-sourcemaps";
+import dartSass from "sass";
+import gulpSass from "gulp-sass";
+
+const sass = gulpSass(dartSass);
+
+gulp.task('server', () => {
+    browserSync({
+        server: {
+            baseDir: './'
+        }
+    });
+
+    gulp.watch('./*.html').on('change', browserSync.reload);
+});
+
+gulp.task('styles', () => {
+    return gulp.src('./scss/*.scss')
+        .pipe(sourceMaps.init())
+        .pipe(sass({}, true).on('error', sass.logError))
+        .pipe(sourceMaps.write('.'))
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream())
+});
+
+gulp.task('watch', () => {
+    gulp.watch('./scss/*.scss', gulp.parallel('styles'));
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
