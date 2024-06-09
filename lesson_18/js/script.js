9d6aad707d5b62526474b9dbce1f7d0c339fe6cc
Oleksandr  Berezhynskyi <alexandr.berezhynskyi@bitfocus.com>
2024-06-09 10:50:26 +0300

    lesson 18 ready

--- lesson_18/js/script.js
+++ lesson_18/js/script.js
@@ -1,12 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     const burgerElement = document.querySelector('.header__burger');
+    const navElement = document.querySelector('.nav');
+    const navItemMenuElements = document.querySelectorAll('.nav__item-menu');
 
     burgerElement.addEventListener('click', event => {
-        console.log(event.target);
-
-        const navElement = document.querySelector('.nav');
-        console.log('navElement', navElement);
-
         navElement.classList.toggle('active');
+        burgerElement.classList.toggle('active');
+    });
+
+    navItemMenuElements.forEach(menuItem => {
+        menuItem.addEventListener('click', event => {
+            navElement.classList.remove('active');
+            burgerElement.classList.remove('active');
+        })
     })
 });
